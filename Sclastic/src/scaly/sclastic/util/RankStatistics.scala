a7ea542039ff2182e4d21f84a2272012ce444f41
Ron Coleman <ronncoleman@gmail.com>
2014-05-23 12:03:05 -0400

    Required lists to be pre-sorted.

--- Sclastic/src/scaly/sclastic/util/RankStatistics.scala
+++ Sclastic/src/scaly/sclastic/util/RankStatistics.scala
@@ -5,6 +5,7 @@ object RankStatistics {
   val PTILE_50 = 1
   val PTILE_75 = 2
   
+  /** Compute rank correlation for sorted lists */
   def correl(xs: List[Double], ys: List[Double]): Double = {
     // Transform data to ranks
     val xranks = rank(xs)
@@ -22,18 +23,15 @@ object RankStatistics {
     rho
   }
   
-  /** Compute the corresponding rank of elements in the array */
-  def rank(xs: List[Double]): List[Double] = {
-    val xsSorted = xs.sort(_ < _)
-    (0 until xsSorted.size).foreach(p => println((p+1)+". "+xsSorted(p)))
-    
+  /** Compute the corresponding rank of elements in the sorted list */
+  def rank(xs: List[Double]): List[Double] = {    
     // Compute the ranks accounting for ties, that is, using
     // the average index if there are ties
     val ranks = xs.map { x =>
       // Find and last indexes of the element
-      val i = xsSorted.findIndexOf(a => a == x) + 1
+      val i = xs.findIndexOf(a => a == x) + 1
       
-      val j = xsSorted.findLastIndexOf(a => a == x) + 1
+      val j = xs.findLastIndexOf(a => a == x) + 1
       
       // k is the number of ties
       val k = j - i + 1
@@ -48,18 +46,16 @@ object RankStatistics {
     ranks
   }
   
-  /** Identify the quantiles of an unsorted distribution */
-  def quantify(xs: List[Double]): List[Double] = {
+  /** Identify the percentiles of a sorted distribution */
+  def quantify(xs: List[Double],ptiles: List[Double]): List[Double] = {
     if(xs.size < 4)
       null
     else {
-      val xsSorted = xs.sort(_ < _)
-      val ptiles = List(0.25,0.50,0.75)
      
       ptiles.foldLeft(List[Double]()) { (as, a) =>
         val indx = (a * xs.size).toInt
        
-        val v = if(indx % 2 != 0) xsSorted(indx-1) else (xsSorted(indx)+xsSorted(indx-1))/2
+        val v = if(indx % 2 != 0) xs(indx-1) else (xs(indx)+xs(indx-1))/2
        
         as ++ List(v)
       }
@@ -72,18 +68,21 @@ object RankStatistics {
   
   /** Test data from Conover (1980) p. 252-253 */
   def test00 {
-    val xs = List(86.,71,77,68,91,72,77,91,70,71,88,87)
-    val ys = List(88.,77,76,64,96,72,65,90,65,80,81,72)
+    val xs = List(86.,71,77,68,91,72,77,91,70,71,88,87).sort(_ < _)
+    val ys = List(88.,77,76,64,96,72,65,90,65,80,81,72).sort(_ < _)
     
     val rho = correl(xs,ys);
     println("rho = "+rho+" expected 0.7378")    
   }
   
+  /** Test data from Conover (1980) p. 252-253 */
   def test01 {
-    val xs = List(86.,71,77,68,91,72,77,91,70,71,88,87)
-    val ys = List(88.,77,76,64,96,72,65,90,65,80,81,72)
+    val xs = List(86.,71,77,68,91,72,77,91,70,71,88,87).sort(_ < _)
+    val ys = List(88.,77,76,64,96,72,65,90,65,80,81,72).sort(_ < _)
     
-    val ptiles = quantify(ys)
-    println(ptiles)
+    val xptiles = quantify(xs,List(0.25,0.50,0.75))
+    val yptiles = quantify(ys,List(0.25,0.50,0.75))
+    println(xptiles)
+    println(yptiles)
   }
 }
